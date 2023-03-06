5c2564dd8fc729fda0bfae10e8daf80d0c041d2b
x-dcx-x <petrisot.guillaume@gmail.com>
2023-03-06 14:20:09 +0100

    commit initial

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,7 +2,7 @@ let start = document.getElementById("start")
 let reset = document.getElementById("reset")
 let choix = document.getElementsByClassName("choix")
 let choiceAnswers = document.getElementsByClassName('choiceAnswers');
-
+let recap = document.getElementById("récapQuestionsOfResponse");
 
 
 /////Table of Questions and responses/////
@@ -15,20 +15,20 @@ let Questions = [
 
     },
     {
-        question: " aux cours de quel année à été inventé le langage C de programmation ?",
-        choiceAnswer: ["  Choix 1: en 1970-1971 ", " Choix 2: 1978  ", " Choix 3:197"],
-        answer: 1
+        question: " aux cours de quel année à été inventé le langage de programmation C ?",
+        choiceAnswer: ["  Choix 1:  1972 ", " Choix 2: 1978  ", " Choix 3:197"],
+        answer: 0
 
     },
 
     {
-        question: "l' update du python 2 aux python3 c est passé durant quel mois ?",
+        question: "l' update du python 2 aux python3 c est passer durant quel mois ?",
         choiceAnswer: [" Choix 1: Avril ", " Choix 2: Octobre  ", " Choix 3: Décembre "],
         answer: 2
 
     },
     {
-        question: " En quel Année le langage MEL à été creer ?",
+        question: " En quel année le langage MEL à été créer ?",
         choiceAnswer: [" Choix 1: 1998 ", " Choix 2: 2000 ", " Choix 3: 1995 "],
         answer: 0
 
@@ -40,7 +40,7 @@ let Questions = [
 
     },
     {
-        question: " Il y a plusieur façon d'ecrire du C++  de manière fonctionnel ?",
+        question: " Il y a plusieurs façons d'écrires du C++  de manière fonctionnel ?",
         choiceAnswer: [" Choix 1: à la main ", " Choix 2:  En pyrogravure   ", " Choix 3: ou en blueprint "],
         answer: 2
 
@@ -52,7 +52,7 @@ let Questions = [
 
     },
     {
-        question: "Quel est l inventeur du Web ?",
+        question: "Quel est l' inventeur du Web ?",
         choiceAnswer: [" Choix 1: Tom Hawk  ", " Choix 2: Tim Berners-Lee ", " Choix 3: Jennifer Hops "],
         answer: 1
 
@@ -64,7 +64,7 @@ let Questions = [
 
     },
     {
-        question: " Netscape communicator est t il un navigageur internet  ?",
+        question: " Netscape communicator est t' il un navigateur internet  ?",
         choiceAnswer: [" Choix 1: oui  ", "Choix 2: peut être ", "Choix 3: Non "],
         answer: 0
 
@@ -72,36 +72,68 @@ let Questions = [
 
 ]
 
-let index = 0 ;
+let index = 0;
+answer = true
+let autresResponse = false
 
-
-
-function clickOfAffichage(){
-    start.addEventListener("click", function (){
+function clickOfAffichage() {
+    start.addEventListener("click", function () {
 
         for (let i = 0; i < choiceAnswers.length; i++) {
             start.innerHTML = "Question suivante";
             let result = document.getElementById("afficheMoiQuestion")
+            if(index > 9) {
+                start.remove();
+                recap.innerHTML ="Qui à crée Javascript ? <br/>" +
+                "Réponse: Choix 1: Brendan Eich\n<br/><br/>" +
+                "\n" +
+                "aux cours de quel année à été inventé le langage de programmation C ?\n<br/>" +
+                "Réponse: Choix 1:  1972 \n<br/><br/>" +
+                "\n" +
+                "l' update du python 2 aux python3 c est passer durant quel mois ?\n<br/>" +
+                "Réponse: Choix 2: Octobre\n<br/><br/><br/>" +
+                "\n" +
+                "En quel année le langage MEL à été créer ?\n<br/>" +
+                "Réponse: Choix 1: 1998\n<br/><br/>" +
+                "\n" +
+                "à quoi fait référence  le langage MEL ?\n<br/>" +
+                "Réponse: Choix 3: à la 3D \n<br/><br/>" +
+                "\n" +
+                "Il y a plusieurs façons d'ecrires du C++  de manière fonctionnel ?\n<br/>" +
+                "Réponse:  Choix 3: ou en blueprint\n<br/><br/>" +
+                "\n" +
+                "Quel est la première page web mise en ligne en 1990 ?\n<br/>" +
+                "Réponse: Choix 1: http://nxoc01.cern.ch/hypertext/WWW/TheProject.html\n<br/><br/>" +
+                "\n" +
+                "Quel est l' inventeur du Web ?\n<br/>" +
+                "Réponse : Choix 2: Tim Berners-Lee\n<br/><br/>" +
+                "\n" +
+                "Avec quel modem ont surfait en 1999 ?\n<br/>" +
+                "Choix 3: un modem 56k \n<br/><br/>" +
+                "\n" +
+                " Netscape communicator est t il un navigateur internet  ?\n<br/>" +
+                " Choix 1: oui";
+            }
             result.innerHTML = Questions[index].question
-
-
             for (let i = 0; i < choix.length; i++) {
                 choix[i].innerHTML = Questions[index].choiceAnswer[i];
-                console.log("blbla");
             }
-         }
-
+        }
     })
 }
+
 clickOfAffichage()
 
-function nextQuestions(){
-for (let i = 0; i < choix.length; i++){
-    choix[i].addEventListener("click", function (){
-        index++;
-        console.log("fhqsdfh");
-
-                })
-            }
+function nextQuestions() {
+    for (let i = 0; i < choix.length; i++) {
+        choix[i].addEventListener("click", function () {
+            index++;
+        })
+    }
 }
+
 nextQuestions()
+reset.addEventListener('click', function () {
+    window.location.reload();
+
+});
